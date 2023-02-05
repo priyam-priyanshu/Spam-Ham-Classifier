eca2d53456b346e22050b640a4f6da96d4062e5c
priyam-priyanshu <76548882+priyam-priyanshu@users.noreply.github.com>
2023-02-05 16:09:49 +0530

    Lemmatized model

--- spamham.py
+++ spamham.py
@@ -1,66 +1,53 @@
 # -*- coding: utf-8 -*-
-# coding: utf-8
-#Naive Bayes
-import os
-import io
-import numpy
-from pandas import DataFrame
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import numpy as np
+import re
+import nltk
+nltk.download('stopwords')
+
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+#ps = PorterStemmer()
+lt = WordNetLemmatizer()
+
+
+
+df = pd.read_csv("Dataset\SMSSpamCollection", sep="\t", names=["labels", "message"])
+
+txt = []
+
+for i in range(len(df)):
+    words = re.sub("[^a-zA-Z]", " ", df["message"][i])
+    words = words.lower()
+    words = words.split()
+    
+    #words = [ps.stem(word) for word in words if word not in stopwords.words('english')]
+    wrods = [lt.lemmatize(word) for word in words if word not in stopwords.words('english')]
+    words = " ".join(words)
+    txt.append(words)
+    
 from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=5000)
+
+x = cv.fit_transform(txt).toarray()
+
+y=pd.get_dummies(df["labels"], drop_first=True)
+
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.20, random_state=0)
+
 from sklearn.naive_bayes import MultinomialNB
+model = MultinomialNB()
 
-#Function to read files (emails) from the local directory
-def readFiles(path):
-    for root, dirnames, filenames in os.walk(path):
-        for filename in filenames:
-            path = os.path.join(root, filename)
+model.fit(xtrain, ytrain)
 
-            inBody = False
-            lines = []
-            f = io.open(path, 'r', encoding='latin1')
-            for line in f:
-                if inBody:
-                    lines.append(line)
-                elif line == '\n':
-                    inBody = True
-            f.close()
-            message = '\n'.join(lines)
-            yield path, message
-
-
-def dataFrameFromDirectory(path, classification):
-    rows = []
-    index = []
-    for filename, message in readFiles(path):
-        rows.append({'message': message, 'class': classification})
-        index.append(filename)
-
-    return DataFrame(rows, index=index)
-
-#An empty dataframe with 'message' and 'class' headers
-data = DataFrame({'message': [], 'class': []})
-
-#Including the email details with the spam/ham classification in the dataframe
-data = data.append(dataFrameFromDirectory('C:/Users/Priyam/Desktop/DecemberBreak/emails/spam', 'spam'))
-data = data.append(dataFrameFromDirectory('C:/Users/Priyam/Desktop/DecemberBreak/emails/ham', 'ham'))
-
-#Head and the Tail of 'data'
-data.head()
-print(data.tail())
-
-vectoriser = CountVectorizer()
-count = vectoriser.fit_transform(data['message'].values)
-print(count)
-
-target = data['class'].values
-print(target)
-
-classifier = MultinomialNB()
-classifier.fit(count, target)
-print(classifier)
-
-exampleInput = ["Hey. This is John Cena. You can't see me", "Free Viagra boys!!", "Please reply to get this offer"]
-excount = vectoriser.transform(exampleInput)
-print(excount)
-
-prediction = classifier.predict(excount)
-print(prediction)
+pred = model.predict(xtest)
